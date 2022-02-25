922c004454349b21528306386d3f2c7d2b91fabd
jorgetunubala <jorgtunubala@gmail.com>
2022-02-25 09:16:24 -0500

    Creando repositorio.

---
+++ src/main/java/com/fundamentosplatzy/springboot/fundamentosSB/FundamentosSbApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzy.springboot.fundamentosSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzy/springboot/fundamentosSB/FundamentosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzy.springboot.fundamentosSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
