b367d70e68b1a48cebc307f6ee2ea7596d10a749
jorgetunubala <jorgtunubala@gmail.com>
2022-03-02 18:28:22 -0500

    Configurar rutas y dependencias.

---
+++ src/main/java/com/fundamentosplatzy/springboot/fundamentosSB/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.fundamentosplatzy.springboot.fundamentosSB.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String> function(){
+        return new ResponseEntity<>("Hello world from controller modificado sin reiniciar todo funcionando.", HttpStatus.OK);
+    }
+
+}
